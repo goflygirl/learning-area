755eb9ab0da5b30e097bd461dad4a6f7df769089
Ines Repnik <ines.repnik@gmail.com>
2024-02-12 08:33:39 +0100

    Added a script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+        const existingText = document.querySelector('#message');
+        console.log(existingText);
+
+        const button = document.querySelector('#btn');
+        console.log(button);
+
+        //Event Handler for the button
+      btn.addEventListener('click', () => {
+        alert('The button was clicked!');
+      });
